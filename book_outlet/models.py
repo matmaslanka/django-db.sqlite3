da8dabc5baaeab52a17c629645d77812fa5b4313
matmaslanka <mates94@gmail.com>
2024-02-25 20:05:38 +0100

    Commit with db.sqlite3

---
+++ book_outlet/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.urls import reverse
+from django.utils.text import slugify
+
+# Create your models here.
+
+
+class Book(models.Model):
+    # https://docs.djangoproject.com/en/5.0/ref/models/fields/
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxLengthValidator(5)])  # Id is added automatically!
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+    
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
+    
+
