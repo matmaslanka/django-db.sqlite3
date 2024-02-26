7691a35bfe2fdf34d32a3f3afad92afb3a4d1586
matmaslanka <mates94@gmail.com>
2024-02-26 22:44:39 +0100

    Updating admin section

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
@@ -10,19 +10,19 @@ class Book(models.Model):
     # https://docs.djangoproject.com/en/5.0/ref/models/fields/
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinLengthValidator(1), MaxLengthValidator(5)])  # Id is added automatically!
+        validators=[MinValueValidator(1), MaxValueValidator(5)])  # Id is added automatically!
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True) # Harry Potter 1 => harry-potter-1
 
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
     
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
