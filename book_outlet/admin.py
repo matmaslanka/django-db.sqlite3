b233a673ce66569bf583088141186207befa3611
matmaslanka <mates94@gmail.com>
2024-02-27 18:55:30 +0100

    adding admin in models

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
-    list_filter =("author","rating",)
-    list_display = ("title","author", )
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author", )
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,15 +6,24 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+
 class Book(models.Model):
     # https://docs.djangoproject.com/en/5.0/ref/models/fields/
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])  # Id is added automatically!
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")                        #CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True) # Harry Potter 1 => harry-potter-1
-
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
@@ -23,6 +32,7 @@ class Book(models.Model):
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
     #     super().save(*args, **kwargs)
+    # To było odpowiedzialne za autogenerowanie slug
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
